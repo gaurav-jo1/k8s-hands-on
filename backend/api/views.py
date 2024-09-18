f6f17e6ea900249b42d3b51db9b25731d69f12c7
Gaurav Joshi <gj451026@gmail.com>
2024-09-18 21:32:04 +0530

    k8s updated

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 def get_hostname(request):
     hostname = socket.gethostname()
-    return HttpResponse(f"Version 2: Hello from the {hostname}")
+    return HttpResponse(f"Version 2: Hello from the {hostname}\n")
